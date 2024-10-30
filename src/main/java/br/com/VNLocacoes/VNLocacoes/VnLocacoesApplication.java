b8c9ba0113fd4565af63dd02ebcc92834fa3868e
vitoriaNay <Vitorianayy6@gmail.com>
2024-10-30 14:02:09 -0300

    primeiro commit

---
+++ src/main/java/br/com/VNLocacoes/VNLocacoes/VnLocacoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.VNLocacoes.VNLocacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnLocacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnLocacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/VNLocacoes/VNLocacoes/VnLocacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.VNLocacoes.VNLocacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VnLocacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
