48b89dc37d56329e5f10858f6267de1652965f5d
Anderson Campos <anderson.camposs87@outlook.com>
2024-12-10 10:49:08 -0300

    Desativando a camada de segurança

--- src/main/java/br/com/VNLocacoes/VNLocacoes/config/SecurityConfig.java
+++ src/main/java/br/com/VNLocacoes/VNLocacoes/config/SecurityConfig.java
@@ -18,8 +18,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private SecurityFilter securityFilter;
+    /*@Autowired
+    private SecurityFilter securityFilter;*/
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -27,15 +27,15 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                    /*.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                     .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                     .requestMatchers(HttpMethod.POST, "/servicos/carro").hasRole("ADMIN")
                     .requestMatchers(HttpMethod.DELETE, "/servicos/carro/{id}").hasRole("ADMIN")
                     .requestMatchers(HttpMethod.PUT, "/servicos/carro/{id}").hasRole("ADMIN")
-                    .requestMatchers(HttpMethod.POST, "/cliente").hasRole("ADMIN")
-                    .anyRequest().authenticated()
-            )
-            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+                    .requestMatchers(HttpMethod.POST, "/cliente").hasRole("ADMIN")*/
+                    .anyRequest().permitAll()
+            );
+            //.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
--- src/main/java/br/com/VNLocacoes/VNLocacoes/config/SecurityFilter.java
+++ src/main/java/br/com/VNLocacoes/VNLocacoes/config/SecurityFilter.java
@@ -15,7 +15,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component
+//@Component
 public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
